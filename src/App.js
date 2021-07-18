38e6c0ed703c8403f354a50c5f6ab3bbeb6fd3e9
Fabio Dias <mfabiodias@gmail.com>
2021-07-18 01:36:38 -0300

    Adicionado adversário robo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { getWinner, winCheck, getSugestion } from './helpers';
 import './App.css';
 
 function App() {
 
   const boardData = Array(9).fill("");
   const player = ["O", "X"];
+  const robot = "X";
   const [board, setBoard] = useState(boardData);
-  const [currentPlayer, setCurrentPlayer] = useState(player[0]);
+  const [currentPlayer, setCurrentPlayer] = useState(player[Number(!player.indexOf(robot))]);
   const [winner, setWinner] = useState(null);
+  const [level, setLevel] = useState("1");
 
-  // Lógica do jogo
+  // Eventos dos jogadores
   const handleClick = (idx) => {
-    // Vencedor ou empate
+    // Vencedor ou Empate
     if(winner || board[idx] !== "") return null;
 
     // Desenha opção do jogador
@@ -21,56 +24,67 @@ function App() {
     setCurrentPlayer(player[Number(!player.indexOf(currentPlayer))]);
   }
 
+  // Mudar dificuldade do jogo
+  const handleChange = (e) => {
+    setLevel(e.target.value);
+    resetGame();
+  }
+
+  // Lógica do robo
+  const autoGame = () => {
+    const sugestion = getSugestion(board, level);
+    handleClick(sugestion);
+  }
+
+  // Reiniciar partida
   const resetGame = () => {
-    setCurrentPlayer(winner !== "E" ? winner : "X");
+    setCurrentPlayer(winner !== "E" && !!winner ? winner : player[Number(!player.indexOf(robot))]);
     setBoard(boardData);
     setWinner(null);
   }
 
+  // Lógica do jogo
   const checkWinner = () => {
-    const winPossible = [
-      // Horizontal
-      [board[0], board[1], board[2]],
-      [board[3], board[4], board[5]],
-      [board[6], board[7], board[8]],
-
-      // Vertical
-      [board[0], board[3], board[6]],
-      [board[1], board[4], board[7]],
-      [board[2], board[5], board[8]],
-
-      // Transversal
-      [board[2], board[4], board[6]],
-      [board[0], board[4], board[8]],
-    ];
-
-    winPossible.forEach(cells => {
+    let winner = "";
+    
+    // Checar se existe um vencedor
+    winCheck(board).forEach(cells => {
       if(cells.every(cell => cell === "X")) {
-        console.log("X venceu"); 
-        setWinner("X");
+        winner = "X";
+        setWinner(winner);
       } else if(cells.every(cell => cell === "O")) {
-        console.log("O venceu"); 
-        setWinner("O");
-      } 
+        winner = "O";
+        setWinner(winner);
+      }
     });
-
-    if(board.every(option => option !== "")) {
-      setWinner("E");
+    
+    // Verifica se existe um empate ou realiza jogada do robo
+    if(!winner) {
+      if(board.every(option => option !== "")) {
+        setWinner("E");
+      } else if(currentPlayer === robot) {
+        autoGame();
+      }
     }
   }
 
-  useEffect(checkWinner, [board]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(checkWinner, [board, currentPlayer]);
 
 
   return (
     <>
       <header>
         <h1 className="title">Jogo da Velha</h1>
+        <select className="custom-select" onChange={handleChange} defaultValue={level} >
+          <option value="1">Fácil</option>
+          <option value="2">Médio</option>
+          <option value="3">Difícil</option>
+        </select>
       </header>
 
       <main>
         <div className={`board ${winner ? "game-over" : ""}`} >
-
           {board.map((item,idx) => (
             <div key={idx} className={`cell ${item}`} onClick={() => handleClick(idx)}>
               {item}
@@ -82,11 +96,10 @@ function App() {
       <footer>
         <h2 className="winner-message">
           <span className={!winner ? `player ${currentPlayer}` : winner }>
-            {!winner ? ` ${currentPlayer} é sua vez!!` : (winner !== "E" ? `${winner} venceu!` : `Empatou!`)} 
+            {getWinner(winner, currentPlayer, robot)}
           </span>
         </h2>
-
-        <button className="reset-button" onClick={resetGame}>Recomeçar o Jogo</button>
+        {winner && <button className="reset-button" onClick={resetGame}>Recomeçar o Jogo</button>}
       </footer>
     </>
   );
---
+++ src/helpers/index.js
@@ -0,0 +1,92 @@
+export const getWinner = (winner, currentPlayer, robo) => {
+    if(!winner) {
+        return currentPlayer === robo ? "É a vez da máquina!" : "É a sua vez!";
+    } else {
+        return winner !== "E" ? `${winner === robo ? "Máquina" : "Você" } venceu!` : `Empatou!`;
+    }
+} 
+
+export const winCheck = (board) => [
+    // Horizontal
+    [board[0], board[1], board[2]],
+    [board[3], board[4], board[5]],
+    [board[6], board[7], board[8]],
+
+    // Vertical
+    [board[0], board[3], board[6]],
+    [board[1], board[4], board[7]],
+    [board[2], board[5], board[8]],
+
+    // Transversal
+    [board[2], board[4], board[6]],
+    [board[0], board[4], board[8]],
+];
+
+export const getSugestion = (board, level) => {
+    const machine = [];
+    const human   = [];
+    const options = [];
+
+    const machineNextOptions = { "1": [], "2": [], "3": [] };
+    const humanNextOptions   = { "1": [], "2": [], "3": [] };
+
+    const winModel = [
+        [0,1,2], [3,4,5], [6,7,8], // Horizontal
+        [0,3,6], [1,4,7], [2,5,8], // Vertical
+        [2,4,6], [0,4,8] // Transversal
+    ];
+
+    board.forEach((v,i) => {
+        if(v === 'O') human.push(i)
+        else if(v === 'X') machine.push(i)
+        else if(v === '') options.push(i)
+    });
+
+    // Máquina
+    let machineOptions = winModel.filter((machineOption, i) => {
+      return !(machineOption.filter(x => human.includes(x))).length
+    });
+
+    machineOptions = machineOptions.map((winOption,i) => {
+        return winOption.filter(x => options.includes(x));
+    });
+
+    machineOptions.forEach(options => {
+      if(!!options.length) machineNextOptions[options.length].push(options)
+    });
+
+    // Humano
+    let humanOptions = winModel.filter((humanOption, i) => {
+      return !(humanOption.filter(x => machine.includes(x))).length
+    });
+
+    humanOptions = humanOptions.map((winOption,i) => {
+        return winOption.filter(x => options.includes(x));
+    });
+
+    humanOptions.forEach(options => {
+      if(!!options.length) humanNextOptions[options.length].push(options);
+    });
+
+    if(!!humanNextOptions[1].length) machineNextOptions[1] = humanNextOptions[1];
+
+    let sugestion;
+    if(parseInt(level) === 1) {
+      sugestion = options;
+    } else if(parseInt(level) === 2) {
+      sugestion = 
+        !!machineNextOptions[1].length ? machineNextOptions[1].flat(1) : 
+        !!machineNextOptions[2].length ? machineNextOptions[2].flat(1) : 
+        !!machineNextOptions[3].length ? machineNextOptions[3].flat(1) : options;
+    } else {
+      sugestion = 
+        !machine.includes(4) && !human.includes(4) ? [4] :
+        !!machineNextOptions[1].length ? machineNextOptions[1].flat(1) : 
+        !!machineNextOptions[2].length ? machineNextOptions[2].flat(1) : 
+        !!machineNextOptions[3].length ? machineNextOptions[3].flat(1) : options;
+    }
+
+    sugestion = sugestion[Math.floor(Math.random()*sugestion.length)]; 
+
+    return sugestion;
+}
